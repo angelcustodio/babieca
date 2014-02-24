e66d2b0933f15cc908f2888fc902f969303edcfd
Angel Custodio <angel@wuaki.tv>
2014-02-24 03:58:57 +0100

    Expiration and more date stuff, neigh

--- model.js
+++ model.js
@@ -1,10 +1,26 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     positionSchema = new Schema({
-      content_id: Number,
-      content_type: String,
       user_id: Number,
-      lapse: Number
+      content_id: Number,
+      content_type: {
+        type: String,
+        enum: ['movie', 'episode']
+      },
+      transaction_type: {
+        type: String,
+        enum: ['rental', 'purchase', 'subscription']
+      },
+      lapse: Number,
+      firstbeat_time: Date,
+      lastbeat_time: Date,
+      beats: Number,
+      // Add the capability to modify this depending on the content_type
+      created_at: {
+        type: Date,
+        default: Date.now(),
+        expires: 100
+      }
     });
 
 module.exports = mongoose.model('ResumePlayback', positionSchema);
--- routes.js
+++ routes.js
@@ -2,7 +2,6 @@ module.exports = function(app){
 
   var ResumePlayback = require('./model');
 
-  // GET All
   allPositions = function(request, response){
     ResumePlayback.find(function(error, positions){
       if(!error)
@@ -12,7 +11,6 @@ module.exports = function(app){
     });
   };
 
-  // GET Position ID
   getPosition = function(request, response){
     ResumePlayback.findOne({
         'user_id': request.params.user_id,
@@ -25,14 +23,16 @@ module.exports = function(app){
       });
   };
 
-  // POST a new position
   newPosition = function(request, response){
 
     var position = new ResumePlayback({
-      content_id:   request.body.content_id,
-      content_type: request.body.content_type,
-      user_id:      request.body.user_id,
-      lapse:        request.body.lapse
+      user_id:          request.body.user_id,
+      content_id:       request.body.content_id,
+      content_type:     request.body.content_type,
+      transaction_type: request.body.transaction_type,
+      lapse:            request.body.lapse,
+      firstbeat_time:   Date.now(),
+      beats:            '1'
     });
 
     position.save(function(error){
@@ -43,10 +43,13 @@ module.exports = function(app){
 
   };
 
-  // PUT a position
   updatePosition = function(request, response){
     ResumePlayback.findById(request.params.id, function(error, position){
-      position.lapse = request.body.lapse;
+
+      position.lapse          = request.body.lapse;
+      position.lastbeat_time  = Date.now();
+      position.beats          = position.beats+1;
+
       position.save(function(error){
         if(error) console.log('ERROR: ' + error);
       });
